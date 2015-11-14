736611ca71920cb97a74c817cd0ce7ef698db7e7
Artem Demo <artemdemo@gmail.com>
2015-11-14 21:36:52 +0300

    Transactions examples

--- build/bitcoin-app.js
+++ build/bitcoin-app.js
@@ -21702,8 +21702,6 @@ return jQuery;
 var bitcoin = require('bitcoinjs-lib');
 var $ = require('jquery-browserify');
 
-console.log(bitcoin.networks);
-
 // Testnet main network
 var network = bitcoin.networks.testnet;
 var keyPair = bitcoin.ECPair.makeRandom({ network: network });
@@ -21719,8 +21717,6 @@ listLog('Public key: ' + keyPair.getAddress());
  * 1.
  * cQ3JdUMMbtUW8ibfLXWXZhRp6HFBBLk3Nqog5QssekY7df8y8ZUk
  * msxMqjP2bzBSdi5sg8zhFNxduK7FZvteEG
- * 10.00
- * transaction hash: f25f1d169385772179de01a37d534a9ab1c30c4fdae53cf7a7de4195d6d26af7
  * 
  * 2.
  * cMwRn7qSPTAAFKC9zCE2k9TADqaeDVhNje33q8RxTqJxxYA2ytzT
@@ -21730,19 +21726,19 @@ listLog('Public key: ' + keyPair.getAddress());
 
 $('.create-transaction').click(function(){
     var network = bitcoin.networks.testnet;
-    var mainKeyPair = bitcoin.ECPair.fromWIF('cQbLrQzkB7cZTJUJ2LQAuyTjDGebRcDGxuZPxQhdkWbLhzkmpa66', network);
+    var mainKeyPair = bitcoin.ECPair.fromWIF('cQ3JdUMMbtUW8ibfLXWXZhRp6HFBBLk3Nqog5QssekY7df8y8ZUk', network);
     var tx = new bitcoin.TransactionBuilder(network);
 
     var changeKey = mainKeyPair.getAddress();
 
     // Add the input (who is paying) of the form [previous transaction hash, index of the output to use]
-    tx.addInput('65a5a4f351fa390561bd73ca6696cdda544d76e1f9f3ee22241a50c1431bae8b', 0);
+    tx.addInput('32e1c22063b980e244ec18509fe4e082e65d3b3237178e7713e89757085d5738', 0);
 
     // Add the output (who to pay to) of the form [payee's address, amount in satoshis]
-    tx.addOutput('n1GxASqueyxzKtj1XM55MmYH37XPq1TtbZ', 7000);
+    tx.addOutput('mqbFSc11N4wyYtNRQRcQSVkxsWULSCCKdc', btcToSatoshi(7));
 
     // Change will be sen here
-    tx.addOutput(changeKey, 3000);
+    // tx.addOutput(changeKey, btcToSatoshi(3));
 
     // Sign the first input with the new key
     tx.sign(0, mainKeyPair);
@@ -21750,6 +21746,15 @@ $('.create-transaction').click(function(){
     // Print transaction serialized as hex
     listLog('Transaction: ' + tx.build().toHex());
 });
+
+/**
+ * Convert BTC to satoshi
+ * 1 BTC = 100,000,000 satoshi
+ *
+ */
+function btcToSatoshi (btc) {
+    return btc * 100000000
+}
 
 function listLog (message) {
     var $message = $(document.createElement('li')).text(message);
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ Testing and checking functionality
 
 ## Instal bitcoin core
 
-Download [bitoicn core](https://bitcoin.org/en/download) and extract it anywhere, for example here `/usr/local/bin/bitcoind/bin/bitcoind`
+Download [bitcoin core](https://bitcoin.org/en/download) and extract it anywhere, for example here `/usr/local/bin/bitcoind/bin/bitcoind`
 
 
 ## Create new .bitcoin folder
@@ -57,7 +57,11 @@ Get available balance
 Should be `50.00`
 
 
-## Transaction and get balance
+## Performing transactions and retrieving balance
+
+Example of custom key pare:
+*Pivate:* cQ3JdUMMbtUW8ibfLXWXZhRp6HFBBLk3Nqog5QssekY7df8y8ZUk
+*Public:* msxMqjP2bzBSdi5sg8zhFNxduK7FZvteEG
 
 Send bitcoins from pull to the address
 ```
@@ -67,4 +71,35 @@ Send bitcoins from pull to the address
 Get amount of bitcoin address
 ```
 /usr/local/bin/bitcoind/bin/bitcoin-cli -regtest getbalance msxMqjP2bzBSdi5sg8zhFNxduK7FZvteEG
+```
+
+Returns array of unspent transaction inputs in this wallet
+```
+/usr/local/bin/bitcoind/bin/bitcoin-cli -regtest listunspent
+```
+This list will output only confirmed transactions. Therefore it will be empty now, to get list of unconfirmed transactions use same command with 0:
+```
+/usr/local/bin/bitcoind/bin/bitcoin-cli -regtest listunspent 0
+```
+
+Import private key to the wallet:
+```
+/usr/local/bin/bitcoind/bin/bitcoin-cli -regtest importprivkey cQ3JdUMMbtUW8ibfLXWXZhRp6HFBBLk3Nqog5QssekY7df8y8ZUk
+```
+
+In order to confirm last transactions we need to generate new block:
+```
+/usr/local/bin/bitcoind/bin/bitcoin-cli -regtest generate 1
+```
+
+## RAW transactions
+
+Decode transaction
+```
+/usr/local/bin/bitcoind/bin/bitcoin-cli -regtest decoderawtransaction [..raw..]
+```
+
+Send raw transaction
+```
+/usr/local/bin/bitcoind/bin/bitcoin-cli -regtest sendrawtransaction [..raw..]
 ```
